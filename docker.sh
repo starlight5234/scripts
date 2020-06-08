d2203962d2ddda546ab19f0e5d3540946710d07d
starlight5234 <ifist2834@gmail.com>
2020-06-09 03:38:28 +0530

    docker: Update docker script

--- docker.sh
+++ docker.sh
@@ -1,22 +1,15 @@
-# Please mention stuff here ( for not vince just change DEVICE )
-DEVICE="vince" # device codename in all small plox
-CONFIG="" # Your kernels defconfig also mention your DEVICE
-CHANNEL_ID="-405830689" # Your channel where you want kernel to be posted
+# Please mention stuff here
+DEVICE="$DEV" # device codename in all small plox
+CONFIG="$CONF" # Your kernels defconfig ( not needed to mention for vince and lavender )
+CHANNEL_ID="$ID" # Your channel where you want kernel to be posted
 TELEGRAM_TOKEN="$BOT_API_KEY" # API Token of YOUR bot ( make sure bot is admin ofc )
 TC_PATH="$HOME/toolchains" # Name your toolchain folder { I don't mess with this }
 ZIP_DIR="$HOME/Zipper"
-CCACHE="no" # yes or no (clang compiler only)
-USE_CLANG="yes"
-
-# Wanna setup ccache?
-if [ $CCACHE == "no" ]; then
-	export CLANG="${TC_PATH}/clang/bin/clang"
-else
-	export CLANG="ccache ${TC_PATH}/clang/bin/clang"
-fi
+USE_GCC="no"
+IS_MIUI="no"
 
 # Device Check
-if [ $DEVICE == "vince" ]; then
+if [ "$DEVICE" == "vince" ]; then
 	export CONFIG=vince-perf_defconfig
 fi
 
@@ -36,9 +29,6 @@ tg_pushzip()
 	JIP=$ZIP_DIR/$ZIP
 	curl -F document=@"$JIP"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
 			-F chat_id=$CHANNEL_ID
-	SHA=$ZIP_DIR/$ZIP.sha1
-	curl -F document=@"$MD5"  "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-			-F chat_id=$CHANNEL_ID
 }
 
 # Sand Updates
@@ -55,7 +45,6 @@ function tg_sandinfo() {
 # Log output Stuff {logic creds: @madeofgreat}
 BTXT="$HOME/build/buildno.txt" #BTXT is Build number TeXT
 if ! [ -a "$BTXT" ]; then
-	tg_sandinfo "$(echo "Why no build tracker???")"
 	mkdir $HOME/build
 	touch $HOME/build/buildno.txt
 	echo 1 > $BTXT
@@ -71,31 +60,41 @@ echo ${BUILD} > $BTXT
 # Toolchain & Zipper Cloner
 function clone_tc() {
 [ -d ${TC_PATH} ] || mkdir ${TC_PATH}
-if [ $USE_CLANG == "yes" ]; then
-	[ -d ${TC_PATH}/clang ] || git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6443078 ${TC_PATH}/clang && rm -rf ${TC_PATH}/clang/.git
-        export CLANG="${TC_PATH}/clang/bin/clang"
+if [ "$USE_GCC" == "yes" ]; then
+	[ -d ${TC_PATH}/gcc-arm64 ] || git clone --depth=1 https://github.com/Unitrix-Kernel/aarch64-linux-android-4.9.git ${TC_PATH}/gcc-arm64
+
+	[ -d ${TC_PATH}/gcc-arm ] || git clone --depth=1 https://github.com/Unitrix-Kernel/arm-linux-androideabi-4.9.git ${TC_PATH}/gcc-arm
+	export CROSS_COMPILE="${TC_PATH}/gcc-arm64/bin/aarch64-linux-android-"
+	export CROSS_COMPILE_ARM32="${TC_PATH}/gcc-arm/bin/arm-linux-androideabi-"
+	export PATH="${TC_PATH}/gcc-arm64/bin:${TC_PATH}/gcc-arm/bin:{$PATH}"
+	export STRIP="${TC_PATH}/gcc-arm64/bin/aarch64-linux-android-strip"	
+else 
+	[ -d ${TC_PATH}/clang ] || git clone --depth=1 https://github.com/Unitrix-Kernel/unitrix-clang.git ${TC_PATH}/clang
+	export PATH="${TC_PATH}/clang/bin:$PATH"
+	export STRIP="${TC_PATH}/clang/aarch64-linux-gnu/bin/strip"
 fi
-
-[ -d ${TC_PATH}/gcc-arm64 ] || git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 ${TC_PATH}/gcc-arm64 && rm -rf ${TC_PATH}/gcc-arm64/.git
-
-[ -d ${TC_PATH}/gcc-arm ] || git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r50 ${TC_PATH}/gcc-arm && rm -rf ${TC_PATH}/gcc-arm/.git
-
 }
 
 function clone_zipper() {
-if [ $DEVICE == "vince" ]; then
-	git clone https://github.com/starlight5234/Zipper -b master $ZIP_DIR #VINCE 
-else
-	git clone https://github.com/starlight5234/Zipper -b test $ZIP_DIR
+if [ "$DEVICE" == "vince" ]; then
+	git clone https://github.com/Unitrix-Kernel/AnyKernel3.git -b vince $ZIP_DIR
+else 
+	WOT=$(echo "Plox Mention your AnyKernel source")
+	tg_sandinfo "${WOT}"
 fi
 }
 
 build_clang () {
 	make -j$(nproc --all) O=out \
                           ARCH=arm64 \
-                          CC="${CLANG}" \
-                          CLANG_TRIPLE=aarch64-linux-gnu- \
-                          LLVM="llvm-" |& tee -a $HOME/build/build${BUILD}.txt
+			  AR=llvm-ar \
+			  NM=llvm-nm \
+			  OBJCOPY=llvm-objcopy \
+			  OBJDUMP=llvm-objdump \
+			  STRIP=llvm-strip \
+                          CC=clang \
+                          CROSS_COMPILE=aarch64-linux-gnu- \
+			  CROSS_COMPILE_ARM32=arm-linux-gnueabi- |& tee -a $HOME/build/build${BUILD}.txt
 }
 
 build_gcc () {
@@ -126,17 +125,14 @@ wifi_modules () {
 # make flashable zip
 function make_flashable() {
 	cd $ZIP_DIR
-	if [ $DEVICE == "vince" ]; then
-		git checkout master &>/dev/null
-	else
-		git checkout test &>/dev/null
+	if [ "$DEVICE" == "vince" ]; then
+		git checkout vince &>/dev/null
 	fi
 	make clean &>/dev/null
 	cp $KERN_IMG $ZIP_DIR/zImage
-    	tg_sandinfo "$(echo "Zipping the KERNUL. 3. 2. 1. Go BRRRRR lmao")"
-	if [ $BRANCH == "stable" ] || [ $BRANCH == "stable-perf" ]; then
+	if [ "$BRANCH" == "stable" ] || [ "$BRANCH" == "stable-perf" ]; then
 	    make stable &>/dev/null
-	elif [ $BRANCH == "beta" ]; then
+	elif [ "$BRANCH" == "beta" ]; then
 	    make beta &>/dev/null
 	else
 	    make test &>/dev/null
@@ -149,35 +145,35 @@ function make_flashable() {
 
 # Clone some basic stuff
 clone_tc
-[ -d $ZIP_DIR ] || clone_zipper && tg_sandinfo "$(echo "Cloned Toolchain & Zipper. Entering Work Directory")"
+[ -d "$ZIP_DIR" ] || clone_zipper
 
 # Main/Common environment
+COMMIT=$(git log --pretty=format:'"%h : %s"' -1)
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 KERNEL_DIR=$(pwd)
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 CONFIG_PATH=$KERNEL_DIR/arch/arm64/configs/$CONFIG
 VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
-STRIP="${TC_PATH}/gcc-arm64/bin/aarch64-linux-android-strip"
 
 # Setup TC & build kernel when?
-export CROSS_COMPILE="${TC_PATH}/gcc-arm64/bin/aarch64-linux-android-"
-export CROSS_COMPILE_ARM32="${TC_PATH}/gcc-arm/bin/arm-linux-androideabi-"
+
 export KERN_VER=$(echo "$(make kernelversion)")
-make clean && make mrproper 
-tg_sandinfo "$(echo "Env Setup completed. Making kernel for $DEVICE version $KERN_VER SoonTM.
-on Branch: $BRANCH")"
+make mrproper && rm -rf out
+
+tg_sandinfo "$(echo "Kernel for :- <b>$DEVICE</b> 
+Version    :- <b>$KERN_VER</b> 
+on Branch  :- <b>$BRANCH</b>
+Commit	   :- <b>$COMMIT</b>")"
 
 # Make defconfig & kernul 
 DATE=`date`
 BUILD_START=$(date +"%s")
 make O=out ARCH=arm64 "$CONFIG"
 
-# Device specific kernel make command
-if [ $USE_CLANG == "no" ]; then
-	export PATH="${TC_PATH}/gcc-arm64/bin:${TC_PATH}/gcc-arm/bin:{$PATH}"
+# Compiler specific kernel make command
+if [ "$USE_GCC" == "yes" ]; then
 	build_gcc
 else
-	export PATH="${TC_PATH}/clang/bin:${TC_PATH}/gcc-arm64/bin:${TC_PATH}/gcc-arm/bin:{$PATH}"
 	build_clang
 fi
 
@@ -189,11 +185,12 @@ if ! [ -a "$KERN_IMG" ]; then
 	tg_erlog
 	exit 1
 else
-	tg_sandinfo "$(echo "Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds")"
+	tg_sandinfo "$(echo "Build Finished after $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds
+	Sending the image to make flashble")"
 
-	# Zipper check
+	# Zip kernel
 	cd $ZIP_DIR
-	if [ $DEVICE != "vince" ]; then
+	if [ "$IS_MIUI" == "yes" ]; then
 		wifi_modules && make_flashable
 	else
 		make_flashable
